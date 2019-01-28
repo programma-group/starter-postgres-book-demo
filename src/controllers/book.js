b1899c2fa5c4095b5f202a726d064090998e1d09
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-01-28 17:45:18 -0400

    Allowing eager loading of reviews

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -5,10 +5,12 @@ const searchBooks = async (req, res) => {
   const data = await Book
     .query()
     .skipUndefined()
+    .allowEager('reviews')
     .where('id', req.query.id)
     .where('asin', req.query.asin)
     .where('description', 'ilike', description ? `%${description}%` : undefined)
     .where('title', 'ilike', title ? `%${title}%` : undefined)
+    .eager(req.query.eager)
     .limit(100);
   return res.send({
     ok: true,
