739226764467eab8f56db2279c9da63fb263807a
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-01-29 10:35:53 -0400

    Using DATABASE_URL environment variable

--- src/knexfile.js
+++ src/knexfile.js
@@ -3,19 +3,13 @@ require('dotenv').config({
 });
 
 const {
-  DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_MIN_POOL, DB_MAX_POOL, DB_PORT,
+  DB_MIN_POOL, DB_MAX_POOL, DATABASE_URL,
 } = process.env;
 
 module.exports = {
   development: {
     client: 'postgresql',
-    connection: {
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASS,
-      database: DB_NAME,
-      port: DB_PORT,
-    },
+    connection: DATABASE_URL,
     pool: {
       min: parseInt(DB_MIN_POOL, 10),
       max: parseInt(DB_MAX_POOL, 10),
